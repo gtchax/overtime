d6e2ec9f118c003c6ea0ba562fb51fc062ae3066
Gwinyai <gtchax@gmail.com>
2017-11-02 00:29:11 +0800

    Implemented custom full name to user

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -5,6 +5,7 @@ describe 'navigate' do
         @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
         login_as(@user, :scope => :user)
     end
+    
     describe 'index' do
 
         before do
@@ -22,8 +23,8 @@ describe 'navigate' do
 
 
         it 'has a list of posts' do
-            post1 = Post.create(date: Date.today, rationale: "Post1")
-            post2 = Post.create(date: Date.today, rationale: "Post2")
+            post1 = Post.create(date: Date.today, rationale: "Post1", user_id: @user.id)
+            post2 = Post.create(date: Date.today, rationale: "Post2", user_id: @user.id)
             visit posts_path
             expect(page).to have_content(/Post1|Post2/)
         end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe "creation" do
-    before do
+  before do
         @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
     end
+
+  describe "creation" do
+   
     it "can be created" do
       expect(@user).to be_valid
     end
@@ -14,5 +16,14 @@ RSpec.describe User, type: :model do
       @user.last_name = nil
       expect(@user).to_not be_valid
     end
+
   end
+
+  describe "cutom name methods" do
+    it "has a full name method that combines first and last name" do
+      expect(@user.full_name).to eq("SNOW, JON")
+    end
+  end
+
+
 end
